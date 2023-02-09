e03c35445fe96280e099636332d67cbfeee8cf19
Jenna Tran <jennatran0205@gmail.com>
2023-02-08 23:08:25 -0700

    calculatorrrr

---
+++ calculatorProgram.py
@@ -0,0 +1,84 @@
+#calculatorProgram.py
+
+"""
+Title: Calculator Program
+Author: Jenna Tran
+Date Created: 2023-02-08
+"""
+
+#---INPUT---#
+def getNumber():
+    """
+    This function will retreive a number from the user
+    """
+    userinput = input("insert number:")
+    userinput = isNumeric(userinput)
+    if userinput == False:
+        return getNumber()
+    else:
+        return userinput
+
+def operationChoice():
+    """
+    user picks which operation to use
+    """
+    print('''
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division
+    ''')
+    choice = input("> ")
+    choice = isNumeric(choice)
+    if choice > 0 and choice < 5:
+        return choice
+    else:
+        print("Sorry, please choose a valid number")
+        return operationChoice()
+            
+
+
+#---PROCESSING---#
+
+def isNumeric(userinput):
+    """
+    This function will check if user input is valid.
+    """
+
+    try:
+        userinput = int(userinput)
+        return userinput
+    except:
+        print("Please pick a valid number.")
+        return False
+    
+
+def addition(number1, number2):
+    """
+    this function will add two variables
+    """
+    answer = number1 + number2
+    return answer
+        
+
+
+#---OUTPUT---#
+
+def displayAnswer(answer):
+    """
+    function will display answer to the user
+    """
+    print("The solution to your calculation is : ", answer)
+
+
+
+
+#---MAIN PROGRAM CODE---#
+
+while True:
+    input1 = getNumber()
+    input2 = getNumber()
+    choice = operationChoice()
+    if choice == 1:
+        answer = addition(input1, input2)
+    displayAnswer(answer)
---
+++ functionExample.py
@@ -0,0 +1,35 @@
+#functionExample.py
+
+"""
+Title: Function Examples
+Author: Jenna Tran
+Date Created: 2023-02-08
+"""
+
+"""
+f(x) = 3x + 2
+x=2
+3(2) + 2 = 8
+f(x) = 8
+
+def = f(x)
+
+"""
+
+def helloworld():
+    """
+    print hello world
+    """
+    print("hello world")
+
+
+def f(x,y):
+    number = x * y
+    z = 40 + 12 - 13
+    return number
+
+
+inputvalue = 2
+inputvalue2 = 4
+number = f(inputvalue, inputvalue2)
+print(number)
